a191fedb130012fbb1d1484478d06d1ad9fc8093
Brandon Williams <brandonwilliams202@outlook.com>
2022-01-30 18:21:42 -0500

    cleanup up project

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # BluetoothRobot
+
+## Overview
+This project allows for an Arduino robot (or any arduino board) to be controlled by a computer via Bluetooth. 
+
+This project contains Python code and Arduino code. The python code is responsible for detecting nearby bluetooth devices, then connecting to the selected arduino device via bluetooth and then sending data to it. The arduino code is reponsible for listening for incoming data and perform arduino actions in response, such as activating servos or motors to move.
+
+But of course this project be instantly applied to communicate with any arduino board with a bluetooth module and not just an entire robot. 
+
+## Requirements
+- Python
+- Arduino robot: This is primarily made of an Arduino Uno board and motors/servos.
+- Arduino bluetooth module conected to the arduino board. 
+- Arduino IDE
+
+## Usage
+1. Upload the ble.ino file to your arduino board using the Ardiono IDE
+2. Turn on the arduino robot, so that the bluetooth module is active and waiting to pair.
+3. Open a terminal and navigate to this project directory
+4. Launch the python script: $ python Main.py
+5. Follow the on-screen steps by selecting the bluetooth module that has been detected by the python program.
+- Note: If the bluettoth module is not detected, restart the python program
+6. (Optional) If the bluetooth "Write Characteristic" in the python program is not appropriate to write to for your bluetooth module, then uncomment the code in the setupConnection() method so that you can test sending data to each chracteristic until you find one that works. Then use that charactersitic by setting the variable "writeCharacteristic" to it.
+7. Then write keyboard inputs to send to the robot.
--- ble.ino
+++ ble.ino
@@ -1,10 +1,16 @@
 //libraries
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
 ///////Vars//////////
 char incomingByte;  // incoming data
-#define carSpeed 150
+int forwardDistance = 0;
 Servo myservo;
+#define carSpeed 150
+#define turnSpeed 180
+#define BT_SERIAL_RX 0
+#define BT_SERIAL_TX 1
+SoftwareSerial BluetoothSerial(BT_SERIAL_RX, BT_SERIAL_TX);
 
 ///////////Pins L298N to IO/////////////////
 #define ENA 5
@@ -14,9 +20,14 @@ Servo myservo;
 #define IN3 9
 #define IN4 11
 
+//Ultrasonic to IO
+#define Echo A4
+#define Trig A5
+
 void setup() 
 {
   Serial.begin(9600); 
+  BluetoothSerial.begin(9600);
   myservo.attach(3);     
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -24,6 +35,8 @@ void setup()
   pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT); 
   myservo.write(86);    //86 is straight(physical offeset)
 }
 
@@ -43,18 +56,77 @@ void brake()
   digitalWrite(ENB, LOW);
 }
 
+void left()
+{
+  analogWrite(ENA, turnSpeed);
+  analogWrite(ENB, turnSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void right()
+{
+  analogWrite(ENA, turnSpeed);
+  analogWrite(ENB, turnSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+int sonarDistance()
+{
+  digitalWrite(Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);  
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);   
+  float timeLen = pulseIn(Echo, HIGH);  
+  float distance = timeLen * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)       
+  return (int)distance;
+}
+
 void loop()
 {
+  //send sonar data
+  forwardDistance = sonarDistance();
+  Serial.println(forwardDistance);
+//  BluetoothSerial.write("test");
+  
   if(Serial.available() > 0)// Checks whether data is comming from the serial port
   {
     incomingByte = Serial.read();
 
-    if(incomingByte == '0') {
-       myservo.write(60);
+    //sonar
+    if(incomingByte == 'p') {
+       int angle = myservo.read();
+       if(angle >= 20){
+          myservo.write(angle-10);
+       }
     }
-    if(incomingByte == '1') {
-       myservo.write(120);
+    if(incomingByte == 'o') {
+       int angle = myservo.read();
+       if(angle <= 160){
+          myservo.write(angle+10);
+       }
     }
+    
+    //movement
+    if(incomingByte == 'w') {
+       forward();
+    }
+    if(incomingByte == 'a') {
+       left();
+    }
+    if(incomingByte == 'd') {
+       right();
+    }
+    if(incomingByte == 's') {
+       brake();
+    }
+    
     Serial.print(incomingByte);
   }
 }
